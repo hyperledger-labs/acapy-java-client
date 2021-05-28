4df82f165b398d3dd462379525edaa7c84ba21ae
Philipp Etschel <philipp.etschel@ch.bosch.com>
2021-05-28 18:02:32 +0200

    added missing annotation
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>

--- src/main/java/org/hyperledger/aries/api/out_of_band/ReceiveInvitationFilter.java
+++ src/main/java/org/hyperledger/aries/api/out_of_band/ReceiveInvitationFilter.java
@@ -1,7 +1,10 @@
 package org.hyperledger.aries.api.out_of_band;
 
+import lombok.Builder;
+import lombok.Data;
 import org.hyperledger.aries.api.AcaPyRequestFilter;
 
+@Data @Builder
 public class ReceiveInvitationFilter implements AcaPyRequestFilter {
     private String alias;
     private Boolean autoAccept;
