755c73c39aecb7094d29780b26570f36463fccc6
Philipp Etschel <philipp.etschel@ch.bosch.com>
2021-08-11 18:07:36 +0200

    owasp check unavailable, license header
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>

--- src/main/java/org/hyperledger/aries/api/credentials/CredentialRevokedFilter.java
+++ src/main/java/org/hyperledger/aries/api/credentials/CredentialRevokedFilter.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020-2021 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository at
+ * https://github.com/hyperledger-labs/acapy-java-client
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package org.hyperledger.aries.api.credentials;
 
 import lombok.Builder;
