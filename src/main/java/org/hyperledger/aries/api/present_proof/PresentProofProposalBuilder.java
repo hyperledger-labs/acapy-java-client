eef4fa508fb7e732d7aa7c53c551b843cd4053c7
Philipp Etschel <philipp.etschel@ch.bosch.com>
2021-11-01 16:27:54 +0100

    not final version of the builder
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>

--- src/main/java/org/hyperledger/aries/api/present_proof/PresentProofProposalBuilder.java
+++ src/main/java/org/hyperledger/aries/api/present_proof/PresentProofProposalBuilder.java
@@ -7,14 +7,13 @@
  */
 package org.hyperledger.aries.api.present_proof;
 
+import com.google.gson.JsonObject;
 import lombok.NonNull;
 import org.hyperledger.aries.api.credentials.Credential;
 import org.hyperledger.aries.api.present_proof_v2.V20PresProposalByFormat;
 import org.hyperledger.aries.api.present_proof_v2.V20PresProposalRequest;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Helper class to build a {@link PresentProofProposal}
@@ -47,15 +46,43 @@ public class PresentProofProposalBuilder {
                 .build();
     }
 
-    public static V20PresProposalRequest v2IndyFromCredential(@NonNull String connectionId, @NonNull Credential cred) {
-        PresentProofProposal v1 = fromCredential(connectionId, cred);
+    /**
+     * A v2 indy presentation proposal is build upon a proof request
+     * @param connectionId the connection id
+     * @param cred {@link Credential}
+     * @param requestName optional name of the proof request
+     * @return {@link V20PresProposalRequest}
+     */
+    public static V20PresProposalRequest v2IndyFromCredential(
+            @NonNull String connectionId, @NonNull Credential cred, String requestName) {
+
+        final Map<String, String> attrs = cred.getAttrs();
+        final List<JsonObject> res = new ArrayList<>();
+
+        attrs.forEach((name, value) -> res.add(PresentProofRequest.ProofRequest.ProofRestrictions
+                .builder()
+                        .addAttributeValueRestriction(name, value)
+                        .schemaId(cred.getSchemaId())
+                        .credentialDefinitionId(cred.getCredentialDefinitionId())
+                .build().toJsonObject()));
+
+        PresentProofRequest.ProofRequest.ProofRequestedAttributes requestedAttributes = PresentProofRequest.ProofRequest.ProofRequestedAttributes
+                .builder()
+                .names(new ArrayList<>(attrs.keySet()))
+                .restrictions(res)
+                .build();
+
         return V20PresProposalRequest
                 .builder()
                 .autoPresent(Boolean.TRUE)
                 .connectionId(connectionId)
                 .presentationProposal(V20PresProposalByFormat
                         .builder()
-                        .indy(v1.getPresentationProposal())
+                        .indy(PresentProofRequest.ProofRequest
+                                .builder()
+                                .name(requestName != null ? requestName : "proof-request")
+                                .requestedAttributes(Map.of(UUID.randomUUID().toString(), requestedAttributes))
+                                .build())
                         .build())
                 .build();
     }
