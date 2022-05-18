1efbce858f3e8d0ffd8f55c805defcca199ff33d
Philipp Etschel <philipp.etschel@ch.bosch.com>
2022-05-18 12:28:51 +0200

    Added reactive event handler (#40)
    * first dummy version
    
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>
    
    * basic rx handler
    
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>
    
    * cleanup
    
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>
    
    * pushed walletId filter up.
    
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>
    
    * ws client is now a rx handler
    
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>
    
    * fixed handler list
    
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>
    
    * pmd + spotbugs
    
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>
    
    * readme and auto closable
    
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>

--- src/main/java/org/hyperledger/aries/AriesWebSocketClient.java
+++ src/main/java/org/hyperledger/aries/AriesWebSocketClient.java
@@ -8,7 +8,7 @@
 package org.hyperledger.aries;
 
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Singular;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.WebSocket;
@@ -16,20 +16,27 @@ import org.apache.commons.lang3.StringUtils;
 import org.hyperledger.aries.webhook.AriesWebSocketListener;
 import org.hyperledger.aries.webhook.EventHandler;
 import org.hyperledger.aries.webhook.IEventHandler;
+import org.hyperledger.aries.webhook.ReactiveEventHandler;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
 
 /**
  * ACA-PY Websocket Client: Receives events from aca-py
  */
-public class AriesWebSocketClient extends BaseClient {
+public class AriesWebSocketClient extends ReactiveEventHandler implements AutoCloseable {
 
+    private final OkHttpClient client;
     private final String url;
     private final String apiKey;
     private final String bearerToken;
-    private final IEventHandler handler;
+    private final List<IEventHandler> handler;
+    private final List<String> walletIdFilter;
 
-    @Getter
     private WebSocket webSocket;
 
     /**
@@ -38,16 +45,22 @@ public class AriesWebSocketClient extends BaseClient {
      * @param apiKey Optional: The admin api key if security is enabled
      * @param bearerToken Optional: The Bearer token used in the Authorization header when running in multi tenant mode
      * @param client Optional: {@link OkHttpClient} if null or not set a default client is created
-     * @param handler Optional: Custom event handler implementation, defaults to {@link EventHandler.DefaultEventHandler}
+     * @param handler Optional: None, one or many custom event handler implementations, defaults to {@link EventHandler.DefaultEventHandler}
+     * @param walletIdFilter Optional: Filter events by provided walletId
      */
     @Builder
-    public AriesWebSocketClient(@Nullable String url, @Nullable String apiKey,
-        @Nullable String bearerToken, @Nullable OkHttpClient client, @Nullable IEventHandler handler) {
-        super(client);
+    public AriesWebSocketClient(@Nullable String url,
+                                @Nullable String apiKey,
+                                @Nullable String bearerToken,
+                                @Nullable OkHttpClient client,
+                                @Singular("handler") List<IEventHandler> handler,
+                                @Nullable @Singular("walletId") List<String> walletIdFilter) {
+        this.client = Objects.requireNonNullElseGet(client, OkHttpClient::new);
         this.url = StringUtils.isEmpty(url) ? "ws://localhost:8031/ws" : StringUtils.trim(url);
         this.apiKey = StringUtils.trimToEmpty(apiKey);
         this.bearerToken = StringUtils.trimToEmpty(bearerToken);
-        this.handler = handler != null ? handler : new EventHandler.DefaultEventHandler();
+        this.walletIdFilter = walletIdFilter != null ? Collections.unmodifiableList(walletIdFilter) : null;
+        this.handler = mergeHandler(handler);
         openWebSocket();
     }
 
@@ -55,19 +68,31 @@ public class AriesWebSocketClient extends BaseClient {
         Request.Builder b = new Request.Builder();
         b.url(url);
         if (apiKey != null) {
-            b.header(X_API_KEY, apiKey);
+            b.header(BaseClient.X_API_KEY, apiKey);
         }
         if (bearerToken != null) {
-            b.header(AUTHORIZATION, BEARER + bearerToken);
+            b.header(BaseClient.AUTHORIZATION, BaseClient.BEARER + bearerToken);
         }
         webSocket = client.newWebSocket(
                 b.build(),
-                AriesWebSocketListener.builder().handler(handler).build());
+                AriesWebSocketListener.builder()
+                        .handler(handler)
+                        .walletIdFilter(walletIdFilter)
+                        .build());
     }
 
-    public void closeWebsocket() {
+    @Override
+    public void close() {
         if (webSocket != null) {
-            webSocket.close(1001, "Going Down");
+            webSocket.close(1001, null);
         }
+    }
+
+    @NotNull
+    private List<IEventHandler> mergeHandler(List<IEventHandler> handler) {
+        List<IEventHandler> tempHandler = new ArrayList<>();
+        tempHandler.add(this);
+        tempHandler.addAll(handler.isEmpty() ? List.of(new EventHandler.DefaultEventHandler()) : handler);
+        return Collections.unmodifiableList(tempHandler);
     }
 }
---
+++ src/main/java/org/hyperledger/aries/api/exception/AriesNetworkException.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020-2022 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository at
+ * https://github.com/hyperledger-labs/acapy-java-client
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.aries.api.exception;
+
+public class AriesNetworkException extends RuntimeException {
+
+    public AriesNetworkException(String message) {
+        super(message);
+    }
+}
