174efea0e17a4d0c9a8a6b43a4f82b8da2b2fc59
Philipp Etschel <philipp.etschel@ch.bosch.com>
2022-05-20 17:44:42 +0200

    create sub wallet clients on sub wallet creation
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>

---
+++ src/main/java/org/hyperledger/aries/ClientToTenant.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020-2022 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository at
+ * https://github.com/hyperledger-labs/acapy-java-client
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.aries;
+
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.ToString;
+import org.hyperledger.aries.api.multitenancy.WalletRecord;
+
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class ClientToTenant extends WalletRecord implements AutoCloseable {
+    private final AriesClient restClient;
+    private final AriesWebSocketClient webSocketClient;
+
+    public ClientToTenant (@NonNull AriesClient restClient,
+        @NonNull AriesWebSocketClient webSocketClient, @NonNull WalletRecord walletRecord) {
+        this.restClient = restClient;
+        this.webSocketClient = webSocketClient;
+        this.setCreatedAt(walletRecord.getCreatedAt());
+        this.setKeyManagementMode(walletRecord.getKeyManagementMode());
+        this.setSettings(walletRecord.getSettings());
+        this.setState(walletRecord.getState());
+        this.setToken(walletRecord.getToken());
+        this.setUpdatedAt(walletRecord.getUpdatedAt());
+        this.setWalletId(walletRecord.getWalletId());
+    }
+
+    public AriesClient send() {
+        return restClient;
+    }
+
+    public AriesWebSocketClient receive() {
+        return webSocketClient;
+    }
+
+    @Override
+    public void close() throws Exception {
+        this.webSocketClient.close();
+    }
+}
