710091c67e38d967ba419e8c2f30b68d356e652b
Philipp Etschel <philipp.etschel@ch.bosch.com>
2022-05-25 16:57:08 +0200

    Added PresentationSubmission Class in VP
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>

--- src/main/java/org/hyperledger/aries/api/jsonld/VerifiablePresentation.java
+++ src/main/java/org/hyperledger/aries/api/jsonld/VerifiablePresentation.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2021 - for information on the respective copyright owner
+ * Copyright (c) 2020-2022 - for information on the respective copyright owner
  * see the NOTICE file and/or the repository at
  * https://github.com/hyperledger-labs/acapy-java-client
  *
@@ -19,6 +19,9 @@ import javax.annotation.Nullable;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @see <a href="https://www.w3.org/2018/credentials/v1#VerifiablePresentation">VerifiablePresentation</a>
@@ -28,10 +31,10 @@ import java.util.List;
 @JsonPropertyOrder({ "@context", "type" })
 public class VerifiablePresentation<T extends VerifiableCredential> {
 
-    public static final transient Type VERIFIABLE_CREDENTIAL_TYPE =
+    public static final Type VERIFIABLE_CREDENTIAL_TYPE =
             new TypeToken<VerifiablePresentation<VerifiableCredential>>(){}.getType();
 
-    public static final transient Type INDY_CREDENTIAL_TYPE =
+    public static final Type INDY_CREDENTIAL_TYPE =
             new TypeToken<VerifiablePresentation<VerifiableIndyCredential>>(){}.getType();
 
     @Builder.Default
@@ -40,21 +43,55 @@ public class VerifiablePresentation<T extends VerifiableCredential> {
     @JsonProperty("@context")
     private List<String> context = List.of("https://www.w3.org/2018/credentials/v1");
 
-    @Nullable
-    private String id;
-
     @Builder.Default
     @NonNull @Nonnull
     private List<String> type = List.of("VerifiablePresentation");
 
     @Nullable
+    private String id;
+
+    @Nullable
     @SerializedName("verifiableCredential")
     private List<T> verifiableCredential;
+
+    private PresentationSubmission presentationSubmission;
 
     @Nullable
     private LinkedDataProof proof;
 
     public List<T> getVerifiableCredential() {
         return verifiableCredential != null ? verifiableCredential : new ArrayList<>();
+    }
+
+    /**
+     * aca-py specific content
+     */
+    @Data @Builder @NoArgsConstructor @AllArgsConstructor
+    public static final class PresentationSubmission {
+        private UUID id;
+        private UUID definitionId;
+        private List<DescriptorMap> descriptorMap = List.of();
+
+        @Data @Builder @NoArgsConstructor @AllArgsConstructor
+        public static final class DescriptorMap {
+
+            private static final Pattern PATH_PATTERN = Pattern.compile("^.*?\\[\\D*(\\d+)\\D*].*$");
+
+            private String id;
+            private String format;
+            private String path;
+
+            /**
+             * Assumes that the path is only one level deep e.g. '$.verifiableCredential[0]'
+             * @return path index as {@link Integer} or null
+             */
+            public Integer getPathAsIndex() {
+                if (path == null) {
+                    return null;
+                }
+                Matcher matcher = PATH_PATTERN.matcher(path);
+                return matcher.find() ? Integer.parseInt(matcher.group(1)) : null;
+            }
+        }
     }
 }
