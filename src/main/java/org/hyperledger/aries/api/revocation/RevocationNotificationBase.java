0510c5ea827752dee2e26a970632740297438904
Philipp Etschel <philipp.etschel@ch.bosch.com>
2022-06-09 15:44:06 +0200

    moved revocation info extraction helper from bpa to client
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>

---
+++ src/main/java/org/hyperledger/aries/api/revocation/RevocationNotificationBase.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2020-2022 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository at
+ * https://github.com/hyperledger-labs/acapy-java-client
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.hyperledger.aries.api.revocation;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+import org.apache.commons.lang3.StringUtils;
+
+public abstract class RevocationNotificationBase {
+
+    RevocationInfo expressionToRevocationInfo(@NonNull String expression, @NonNull Integer expLength) {
+        RevocationInfo.RevocationInfoBuilder b = RevocationInfo.builder();
+        if (StringUtils.isNotEmpty(expression)) {
+            String[] parts = expression.split("::");
+            if (parts.length != expLength) {
+                throw new IllegalArgumentException("Not a valid revocation notification");
+            }
+            b.revRegId(parts[expLength - 2]);
+            b.credRevId(parts[expLength - 1]);
+        }
+        return b.build();
+    }
+
+
+    @Data
+    @Builder
+    public static final class RevocationInfo {
+        /** revocation-registry-id */
+        private String revRegId;
+        /** credential-revocation-id */
+        private String credRevId;
+    }
+}
--- src/main/java/org/hyperledger/aries/api/revocation/RevocationNotificationEventV2.java
+++ src/main/java/org/hyperledger/aries/api/revocation/RevocationNotificationEventV2.java
@@ -9,18 +9,30 @@ package org.hyperledger.aries.api.revocation;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.gson.annotations.SerializedName;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 /**
  * webhook event: revocation-notification-v2
+ * aries-rfc: 0721-revocation-notification-v2
  */
-@Data @NoArgsConstructor
-public class RevocationNotificationEventV2 {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(callSuper = true)
+public class RevocationNotificationEventV2 extends RevocationNotificationBase {
 
     private RevocationFormat revocationFormat;
     private String credentialId;
     private String comment;
+
+    /**
+     * Extracts revocation information form the credentialId
+     * @return {@link RevocationInfo}
+     */
+    public RevocationInfo toRevocationInfo() {
+        return expressionToRevocationInfo(credentialId, 2);
+    }
 
     public enum RevocationFormat {
         @JsonProperty("indy-anoncreds")
