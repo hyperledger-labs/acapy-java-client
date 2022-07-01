0f7938051c9e329d83c97a8d63061b5f05bf5518
Philipp Etschel <philipp.etschel@ch.bosch.com>
2022-07-01 09:38:14 +0200

    license header
    Signed-off-by: Philipp Etschel <philipp.etschel@ch.bosch.com>

--- src/main/java/org/hyperledger/aries/api/multitenancy/WalletKeyDerivation.java
+++ src/main/java/org/hyperledger/aries/api/multitenancy/WalletKeyDerivation.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2020-2022 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository at
+ * https://github.com/hyperledger-labs/acapy-java-client
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package org.hyperledger.aries.api.multitenancy;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
